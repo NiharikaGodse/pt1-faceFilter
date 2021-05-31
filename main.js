b45ee7b8e81e1c8398bcf7b05ba8ba8e5000d916
Niharika Godse <71769556+NiharikaGodse@users.noreply.github.com>
2021-05-31 18:10:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(850,500);
+    canvas.position(250,200);
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save('MyFilterPic.jpg');
+}
